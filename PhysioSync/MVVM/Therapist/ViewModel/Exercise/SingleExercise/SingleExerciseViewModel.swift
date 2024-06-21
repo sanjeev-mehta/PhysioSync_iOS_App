5b7cb3367f53a153e53ab136d341eb340e41efa0
rohxoxo <rohitnicbc@gmail.com>
2024-06-20 19:18:15 -0700

    ci: Update Category View Model
    This commit contains the updating code of category view model

---
+++ PhysioSync/MVVM/Therapist/ViewModel/Exercise/SingleExercise/SingleExerciseViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  SingleExerciseViewModel.swift
+//  PhysioSync
+//
+//  Created by Rohit on 2024-06-20.
+//
+
+import UIKit
+
+class SingleExerciseViewModel {
+    
+    // MARK: -  Variables
+    static let shareInstance = SingleExerciseViewModel()
+    let apiHelper = ApiHelper.shareInstance
+    var exerciseModel = [SingleExerciseModel]()
+    
+    func getSingleExercise(vc: UIViewController, name: String, completion: @escaping (Bool) -> ()) {
+        let url = API.Endpoints.getAllExercises + "name=\(name)"
+        apiHelper.getApi(view: vc, url: url, isHeader: true, isLoader: true) { json, err in
+            if let error = err {
+                completion(false)
+            } else {
+                let exercises = json["data"].arrayValue.map { SingleExerciseModel($0) }
+                self.exerciseModel = exercises
+                completion(true)
+            }
+        }
+    }
+    
+    // MARK: - Get Array Count
+    func getArrayCount() -> Int {
+        return exerciseModel.count
+    }
+    
+    func setCell(_ cell: ExerciseGridCVC, index: Int) {
+        let data = exerciseModel[index]
+        cell.titleLbl.text = data.videoTitle
+        cell.imgVW.setImage(with: data.videoUrl)
+    }
+}
