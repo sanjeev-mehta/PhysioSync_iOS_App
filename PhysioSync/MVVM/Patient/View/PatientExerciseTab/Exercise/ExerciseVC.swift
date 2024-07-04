f3e187b726f61964cd3460654d96de87c8df0092
Sanjeev <sanjeevmehta500@gmail.com>
2024-07-04 10:57:22 -0700

    ci: Implementation of Machine learning Model
    This commit contains the implementation of machine learning model of neck rotation

--- PhysioSync/MVVM/Patient/View/PatientExerciseTab/Exercise/ExerciseVC.swift
+++ PhysioSync/MVVM/Patient/View/PatientExerciseTab/Exercise/ExerciseVC.swift
@@ -12,15 +12,24 @@ class ExerciseVC: UIViewController {
     
     //MARK: - Outlets
     @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var repLbl: UILabel!
     
     var previewLayer: AVCaptureVideoPreviewLayer?
     let videoCapture = VideoCapture()
-
+    
     var poseView = PoseView()
+    
+    enum ExerciseStage {
+        case still, right, left
+    }
+    
+    var currentStage: ExerciseStage = .still
+    var repCount = 0
+    var halfRep = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setUpVideoPreview()
         videoCapture.predictor.delegate = self
         
@@ -34,31 +43,65 @@ class ExerciseVC: UIViewController {
         guard let previewLayer = previewLayer else {return}
         
         cameraView.layer.addSublayer(previewLayer)
-        previewLayer.frame = view.frame
+        previewLayer.frame = cameraView.bounds
         
         cameraView.addSubview(poseView)
-        poseView.frame = view.frame
+        poseView.frame = cameraView.bounds
         poseView.backgroundColor = .clear
         
     }
-
+    
 }
 
 extension ExerciseVC: PredictorDelegate {
     
     func predictor(_ predictor: Predictor, didLabelAction action: String, with confidence: Double) {
-//        print("=====>>>>>",action, confidence)
+        guard confidence > 0.85 else { return }
+        print("=====>>>>>",action, confidence)
+        leftRightRepCount(action: action)
     }
     
     func predictor(_ predictor: Predictor, didFindNewRecognizedPoints points: [CGPoint]) {
         guard let previewLayer = previewLayer else { return }
         
-        let convertedPoints = points.map {
-            previewLayer.layerPointConverted(fromCaptureDevicePoint: $0)
+        if points.isEmpty {
+            poseView.points = []
+        } else {
+            let convertedPoints = points.map {
+                previewLayer.layerPointConverted(fromCaptureDevicePoint: $0)
+            }
+            poseView.points = convertedPoints
         }
-        
-        poseView.points = convertedPoints
     }
     
+    func leftRightRepCount(action: String) {
+        switch action {
+        case "still1":
+            if currentStage == .right || currentStage == .left {
+                currentStage = .still
+            }
+        case "right1":
+            if currentStage == .still {
+                halfRep += 1
+                currentStage = .right
+            }
+        case "left1":
+            if currentStage == .still {
+                halfRep += 1
+                currentStage = .left
+            }
+        default:
+            break
+        }
+        
+        if halfRep == 2 {
+            repCount += 1
+            halfRep = 0
+        }
+        DispatchQueue.main.async {
+            self.repLbl.text = "REPS\n \(self.repCount)"
+        }
+        print("Reps: \(repCount)")
+    }
 }
 
